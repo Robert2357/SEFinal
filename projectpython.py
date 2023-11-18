907e62abe6d370900bdaca969e3f3cb77c742a02
rk4758 <148025379+rk4758@users.noreply.github.com>
2023-11-18 00:02:31 -0500

    Update projectpython.py
    Basic working main program (no exception handling added yet)

--- projectpython.py
+++ projectpython.py
@@ -1,18 +1,39 @@
-import math
+#Main calculator program
 
-print("This program is made to calcuate three numbers within a certain range.")
+# Function to add numbers
+def add(num1, num2, num3):
+    return num1 + num2 + num3
+ 
+# Function to multiply two numbers
+def multiply(num1, num2, num3):
+    return num1 * num2 * num3
+ 
+# Function to divide two numbers
+def average(num1, num2, num3):
+    return (num1 + num2 + num3)/3
 
-num1 = input("Enter a number between 1-10: ")
-num2 = input("Enter a number between 20-30: ")
-num3 = input ("Enter a number between 70-100: ")
+#User inputs numbers
+number_1 = int(input("Enter a number 1-10: "))
+number_2 = int(input("Enter a number 20-30: "))
+number_3 = int(input("Enter a number 70-100: "))
 
-print("Now choose your option of calcuation and enter the name. 1 for Sum Calcuation, 2 for GCD calcuation, and 3 for average calcuation." )
+print("Please select operation -\n" \
+        "1. Sum\n" \
+        "2. Average\n" \
+        "3. Product\n")
+ 
+ 
+# User inputs operation choice
+select = int(input("Select operation 1, 2, or 3 :"))
+ 
+if select == 1:
+    print(add(number_1, number_2, number_3))
+ 
+elif select == 2:
+    print((number_1, number_2, number_3))
+ 
+elif select == 3:
+    print(multiply(number_1, number_2, number_3))
 
-option = input("Enter the corresponding number option: ")
-
-if option == 1:
-    print ( "The sum of the three numbers is: " +  (num1 + num2 + num3) )
-elif option == 2:
-    print ("The GCD of the three numbers is: " + math.gcd(num1, num2, num3))
-elif option == 3:
-    print ("The mean of the three numbers is: " + ((num1 + num2 + num3)/3))
+else:
+    print("Invalid input")
